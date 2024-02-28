b8c4a1e4bc0091c8a1e37216518e85ef83e13433
Claudia Pavon <159437150+852698ClaudiaP@users.noreply.github.com>
2024-02-28 13:42:28 +0100

    Updated Card, closed #2
    Card now only loads the sprite when it changes

--- Src/Card.gd
+++ Src/Card.gd
@@ -1,32 +1,38 @@
 extends Node
+class_name Card
 
 @export var value : int
 @export var type : String
 @export var facing_up : bool
 @export var back : int
+@export var front_sprite : Resource
+@export var back_sprite : Resource
 
-@onready var sprite = $Sprite2D
+@onready var sprite : Sprite2D = $Sprite2D
 
 func new(newValue : int, newType : String, newFacingUp : bool, newBack : int) -> void:
 	value = newValue
 	type = newType
 	facing_up = newFacingUp
 	back = newBack
+	front_sprite = load("res://Assets/" + type + "s/" + type + "s_card_" + __value_to_string(value) + ".png")
+	back_sprite = load("res://Assets/Backs/back_" + str(back) + ".png")
 
 func _ready():
-	setFacing(facing_up)
+	set_facing_up(facing_up)
 
-func setVisibility(newVisible : bool) -> void:
-	sprite.visible = newVisible
-
-func setFacing(newFacingUp : bool):
+func set_facing_up(newFacingUp : bool = facing_up):
 	facing_up = newFacingUp
-	if (newFacingUp):
-		sprite.texture = load("res://Assets/" + type + "s/" + type + "s_card_" + int_to_string_number(value) + ".png")
+	if (facing_up):
+		sprite.texture = front_sprite
 	else:
-		sprite.texture = load("res://Assets/Backs/back_" + str(back) + ".png")
+		sprite.texture = back_sprite
 
-func int_to_string_number(val : int) -> String:
+func change_back(newBack : int):
+	back = newBack
+	back_sprite = load("res://Assets/Backs/back_" + str(newBack) + ".png")
+
+func __value_to_string(val : int) -> String:
 	var number = str(val+1)
 	if number.length() == 1:
 		number = "0" + number
