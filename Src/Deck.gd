0895886d3141136a5e8e36af78eeb2807f4dcf29
Claudia Pavon <159437150+852698ClaudiaP@users.noreply.github.com>
2024-02-29 14:47:31 +0100

    closed #1

--- Src/Deck.gd
+++ Src/Deck.gd
@@ -5,16 +5,18 @@ var card = preload("res://Prefabs/Card.tscn")
 var types = ["Spade", "Heart", "Diamond", "Club"]
 
 func _ready():
+	self.position = Vector2(40, 40)
 	for i in range(4):
 		for j in range(13):
 			var newCard = card.instantiate()
-			newCard.position = Vector2(i * 200 + 300, j * 50 + 50)
-			newCard.new(j, types[i], false, randi()%8)
+			newCard.new(j, types[i], false, 0)
 			add_child(newCard)
 
-func dealCard() -> Node:
+func deal_card() -> Node:
 	var childCount = get_child_count()
-	var randomCardIndex = randi() % childCount
-	var randomCard = get_child(randomCardIndex)
-	remove_child(randomCard)
-	return randomCard
+	if childCount > 0 :
+		var randomCardIndex = randi() % childCount
+		var randomCard = get_child(randomCardIndex)
+		remove_child(randomCard)
+		return randomCard
+	return null
